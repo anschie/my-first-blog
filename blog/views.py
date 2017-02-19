9c0159b27cf6c60e8499219d047c3f3525a57ccf
Angie <anbearth@gmail.com>
2017-02-19 12:04:45 +0100

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
+	posts = Post.objects.order_by('published_date')	
 	return render(request, 'blog/post_list.html', {})
 
+#>>> from blog.models import Post
